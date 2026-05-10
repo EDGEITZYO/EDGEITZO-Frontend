9d018401f3f3d658e73e5e97b4618e6a8dc50892
minju3212 <smj3mjs@gmail.com>
2026-05-11 01:53:12 +0900

    ✨ feat: 페이지 컴포넌트 생성 및 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-function App() {
-  return <></>
-}
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
-export default App
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
