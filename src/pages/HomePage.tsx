4fd44a30ad302dd213c377b29b27bcdd4173dddb
minju3212 <smj3mjs@gmail.com>
2026-05-15 21:27:26 +0900

    ✨ feat: BottomNav 컴포넌트 생성

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material';
 import Header from '../components/layout/Header';
-//import BottomNav from '../components/layout/BottomNav';
+import BottomNav from '../components/layout/BottomNav';
 
 const HomePage = () => {
   return (
@@ -21,7 +21,7 @@ const HomePage = () => {
         {/* 개인 맞춤화 메시지 + 검색창 */}
         {/* 최근 탐색 이어하기 + 최근 확인한 논문 */}
       </Box>
-      {/* <BottomNav /> */}
+      <BottomNav />
     </Box>
   );
 };
