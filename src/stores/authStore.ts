49874b12fc60ae4cfccbbe5d7a761ae6412b61dc
minju3212 <smj3mjs@gmail.com>
2026-07-03 16:19:51 +0900

    ♻️ refactor: authStore에서 서버 데이터(userName, userId) 제거

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -3,19 +3,13 @@ import { create } from "zustand";
 interface AuthState {
   accessToken: string | null;
   isAuthenticated: boolean;
-  userName: string | null;
-  userId: string | null;
   setAccessToken: (accessToken: string) => void;
-  setUserName: (name: string) => void;
-  setUserId: (id: string) => void;
   clearAuth: () => void;
 }
 
 export const useAuthStore = create<AuthState>()((set) => ({
   accessToken: null,
   isAuthenticated: false,
-  userName: null,
-  userId: null,
 
   setAccessToken: (accessToken: string) => {
     set({
@@ -24,20 +18,10 @@ export const useAuthStore = create<AuthState>()((set) => ({
     });
   },
 
-  setUserName: (name: string) => {
-    set({ userName: name });
-  },
-
-  setUserId: (id: string) => {
-    set({ userId: id });
-  },
-
   clearAuth: () => {
     set({
       accessToken: null,
       isAuthenticated: false,
-      userName: null,
-      userId: null,
     });
   },
 }));
